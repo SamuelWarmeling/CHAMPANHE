1b49c85b8f5e89e4fed93f084bde647c7c77ece4
SamuelWarmeling <121568852+SamuelWarmeling@users.noreply.github.com>
2026-04-17 22:10:25 -0300

    Integrate Clarity analytics script in app layout
    Added Clarity analytics script to the layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,14 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wddergrgtp");
+</script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
